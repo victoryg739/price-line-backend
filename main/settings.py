f82250854668d1746f3ace4fd7bf35728ac6ee3b
victor <victoryoong570@gmail.com>
2023-03-28 01:47:07 +0800

    settings

--- main/settings.py
+++ main/settings.py
@@ -188,3 +188,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
